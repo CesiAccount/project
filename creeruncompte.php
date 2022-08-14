823010f2cc5dbd4960c9ad4948b239c82dc33fcf
CesiAccount <bot.discordhell@gmail.com>
2022-08-14 02:05:47 +0200

    2h00 14/08/22 creation ok

--- creeruncompte.php
+++ creeruncompte.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_destroy();
 ?>
 
 
@@ -10,20 +11,20 @@ session_start();
 <body>
     <div class=body>
         <form id="formulaire" action=creeruncompte.php method="POST">
+            <div class="username">
+                <input type="text" id="username" name="username" placeholder="Identifiant" required>
+            </div>
+            <div class="key">
+                <input type="password" id="key" name="key" placeholder="Mot de passe" required>
+            </div>
             <div class="nom">
                 <input type="text" id="nom" name="nom" placeholder="Nom" required>
             </div>
             <div class="prenom">
                 <input type="text" id="prenom" name="prenom" placeholder="Prenom" required>
             </div>
-            <div class="id_compte">
-                <input type="text" id="id_compte" name="id_compte" placeholder="Username" required>
-            </div>
             <div class="ville">
                 <input type="text" id="ville" name="ville" placeholder="Ville" required>
-            </div>
-            <div class="key">
-                <input type="password" id="key" name="key" placeholder="Mot de passe">
             </div>
             <div class="submit">
                 <input type="submit" name="save" value="S'inscrire">
@@ -45,14 +46,18 @@ if(isset($_POST['save']))
 {
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
-    $id_compte = $_POST['id_compte'];
     $ville = $_POST['ville'];
+    $username =$_POST['username'];
+    $id=$_POST['id_compte'];
+    $key=$_POST['key'];
 
 
-    $requete = $connexion->prepare("INSERT INTO compte (nom, prenom, id_compte, ville)
-    VALUES (?, ?, ?, ?);");
-    $requete->execute(array($nom, $prenom, $id_compte, $ville));
+    $requete = $connexion->prepare("INSERT INTO login (username, password, nom, prenom, ville)
+    VALUES (?, ?, ?, ?, ?);");
+    $requete->execute(array($username, $key, $nom, $prenom, $ville));
     $connexion->exec($requete);
+    exit();
+    header("location:login.php");
 }
 }
 catch(PDOException $e)
